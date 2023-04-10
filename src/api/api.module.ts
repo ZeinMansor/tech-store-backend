2af0593d4a2354418f1ec429292825f335108dcd
ZeinManssor <zeinmansor90@gmail.com>
2023-04-10 22:09:36 +0300

    Added env config file

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { UserController } from './controllers/user/user.controller';
+import { AuthController } from './controllers/auth/auth.controller';
+import { TypeormService } from './services/typeorm/typeorm.service';
 import { UserService } from './services/user/user.service';
 
 @Module({
   imports: [UserModule],
-  controllers: [UserController],
-  providers: [UserService]
+  controllers: [UserController, AuthController],
+  providers: [UserService, TypeormService]
 })
 export class ApiModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeormService } from './services/typeorm/typeorm.service';
 import { ApiModule } from './api/api.module';
+import { getEnvPath } from './common/helpers/env.helper';
+import { ConfigModule } from '@nestjs/config';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/env`);
 
 @Module({
-  imports: [ApiModule, ],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    ApiModule ],
   controllers: [AppController],
-  providers: [AppService, TypeormService],
+  providers: [AppService],
 })
 export class AppModule {}
